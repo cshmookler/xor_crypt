d971a373bec5efdcbf1f2872d575e756f0d09e09
Caden Shmookler <cshmookler@gmail.com>
2023-11-16 14:04:22 -0700

    Added rudimentary stdin and stdout handling
    stdin behavior appears to work
    stdout behavior has not been tested
    The size of the pad is no longer checked to ensure that it is large enough to encrypt the input file

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,13 @@ constexpr std::string_view proper_usage = {
     "   xorc --help\n"
     "   xorc --version\n"
     "   xorc <input_file> <output_file> [--pad=<path_to_pad>] [--pos="
-    "<position_in_pad>]\n\n"
+    "<position_in_pad>]\n"
+    "   xorc --stdin <output_file> [--pad=<path_to_pad>] "
+    "[--pos=<position_in_pad>]\n"
+    "   xorc --stdout <input_file> [--pad=<path_to_pad>] "
+    "[--pos=<position_in_pad>]\n"
+    "   xorc --stdin --stdout [--pad=<path_to_pad>] "
+    "[--pos=<position_in_pad>]\n\n"
 };
 
 int failure(const std::string_view& msg) {
@@ -43,16 +49,40 @@ class file_open_error : public std::exception {
     }
 };
 
-class file_r {
+enum class eof {
+    file,
+    stdin_, // NOLINT - added underscore to avoid conflicting macros
+};
+
+class eof_error : public std::exception {
+    eof eof_;
+
+  public:
+    explicit eof_error(eof eof) : eof_(eof) {}
+    [[nodiscard]] eof get_eof_type() const noexcept { return this->eof_; }
+    [[nodiscard]] const char* what() const noexcept override {
+        return "Encountered EOF while reading stream.";
+    }
+};
+
+class file_not_open_error : public std::exception {
+    const char* err_;
+
+  public:
+    explicit file_not_open_error(const char* err) : err_(err) {}
+    [[nodiscard]] const char* what() const noexcept override {
+        return this->err_;
+    }
+};
+
+class istream {
     std::ifstream stream_;
     size_t size_ = 0;
 
-    void close_() { this->stream_.close(); }
-
   public:
-    void open(const std::string_view& name, const size_t pos) {
+    void open_file(const std::string_view& name, const size_t pos) {
         if (this->stream_.is_open()) {
-            this->close_();
+            this->close();
         }
         this->stream_.open(
             name.data(),
@@ -66,29 +96,49 @@ class file_r {
         this->stream_.seekg(static_cast<std::streamoff>(pos));
     }
 
-    file_r(const std::string_view& name, const size_t pos) {
-        this->open(name, pos);
+    void close() { this->stream_.close(); }
+
+    istream() = default;
+    istream(const std::string_view& name, const size_t pos) {
+        this->open_file(name, pos);
     }
-    file_r(const file_r&) = delete;
-    file_r(file_r&&) noexcept = default;
-    file_r& operator=(const file_r&) = delete;
-    file_r& operator=(file_r&&) noexcept = default;
-    ~file_r() { this->close_(); }
+    istream(const istream&) = delete;
+    istream(istream&&) noexcept = default;
+    istream& operator=(const istream&) = delete;
+    istream& operator=(istream&&) noexcept = default;
+    ~istream() { this->close(); }
 
-    [[nodiscard]] size_t size() const { return this->size_; }
+    [[nodiscard]] size_t size() const {
+        if (! this->stream_.is_open()) {
+            throw file_not_open_error("Cannot get size of stdin.\n");
+        }
+        return this->size_;
+    }
 
-    u_char get() { return this->stream_.get(); }
+    u_char get() {
+        u_char chr = 0;
+        if (this->stream_.is_open()) {
+            chr = this->stream_.get();
+            if (this->stream_.eof()) {
+                throw eof_error(eof::file);
+            }
+            return chr;
+        }
+
+        if (! (std::cin >> chr)) {
+            throw eof_error(eof::stdin_);
+        }
+        return chr;
+    }
 };
 
-class file_w {
+class ostream {
     std::ofstream stream_;
-
-    void close_() { this->stream_.close(); }
 
   public:
     void open(const std::string_view& name) {
         if (this->stream_.is_open()) {
-            this->close_();
+            this->close();
         }
         this->stream_.open(
             name.data(),
@@ -98,16 +148,23 @@ class file_w {
         }
     }
 
-    file_w() = default;
-    explicit file_w(const std::string_view& name) { this->open(name); }
-    file_w(const file_w&) = delete;
-    file_w(file_w&&) noexcept = default;
-    file_w& operator=(const file_w&) = delete;
-    file_w& operator=(file_w&&) noexcept = default;
-    ~file_w() { this->close_(); }
+    void close() { this->stream_.close(); }
+
+    ostream() = default;
+    explicit ostream(const std::string_view& name) { this->open(name); }
+    ostream(const ostream&) = delete;
+    ostream(ostream&&) noexcept = default;
+    ostream& operator=(const ostream&) = delete;
+    ostream& operator=(ostream&&) noexcept = default;
+    ~ostream() { this->close(); }
 
     void put(const u_char chr) {
-        this->stream_.put(static_cast<std::ofstream::char_type>(chr));
+        if (this->stream_.is_open()) {
+            this->stream_.put(static_cast<std::ofstream::char_type>(chr));
+        }
+        else {
+            std::cout << chr;
+        }
     }
 };
 
@@ -115,6 +172,8 @@ enum class opt_id {
     none,
     help,
     version,
+    stdin_,  // NOLINT - added underscore to avoid conflicting macros
+    stdout_, // NOLINT - added underscore to avoid conflicting macros
     pad,
     pos,
 };
@@ -122,16 +181,19 @@ enum class opt_id {
 struct opt_t {
     std::string_view name;
     opt_id id = opt_id::none;
-    bool value = true;
+    bool value = false;
 };
 
-using opt_arr_t_f = std::array<opt_t, 4>;
+// NOLINTNEXTLINE(readability-magic-numbers)
+using opt_arr_t_f = std::array<opt_t, 6>;
 constexpr opt_arr_t_f option = {
     {
-     { "--help", opt_id::help, false },
-     { "--version", opt_id::version, false },
-     { "--pad=", opt_id::pad },
-     { "--pos=", opt_id::pos },
+     { "--help", opt_id::help },
+     { "--version", opt_id::version },
+     { "--stdin", opt_id::stdin_ },
+     { "--stdout", opt_id::stdout_ },
+     { "--pad=", opt_id::pad, true },
+     { "--pos=", opt_id::pos, true },
      }
 };
 
@@ -141,9 +203,11 @@ struct pad_t {
 };
 
 struct crypt_t {
-    std::string_view input;
-    pad_t pad;
-    std::string_view output;
+    std::string_view input = "data";
+    pad_t pad = { .path = "pad.key", .pos = 0 };
+    std::string_view output = "data.crypt";
+    bool use_stdin = false;
+    bool use_stdout = false;
 };
 
 class show_help : public std::exception {
@@ -161,23 +225,28 @@ class show_version : public std::exception {
 };
 
 crypt_t get_crypt_args(const std::span<char*>& in_cmd_arg) {
-    crypt_t crypt = {
-        .input = "data",
-        .pad = {.path = "pad.key", .pos = 0},
-        .output = "data.crypt"
-    };
-    constexpr opt_t default_opt = { .name = "",
-                                    .id = opt_id::none,
-                                    .value = false };
+    crypt_t crypt{};
+    constexpr opt_t default_opt{};
     opt_t found_opt = default_opt;
     size_t cmd_arg_i = 0;
+    bool use_input_file = false;
+    bool use_output_file = false;
 
     for (std::string_view arg : in_cmd_arg) {
         if (arg.rfind("--", 0) != 0) {
             // positional argument
             switch (cmd_arg_i) {
-                case 0: crypt.input = arg; break;
-                case 1: crypt.output = arg; break;
+                case 0:
+                    if (! crypt.use_stdin) {
+                        crypt.input = arg;
+                        use_input_file = true;
+                        break;
+                    }
+                    [[fallthrough]];
+                case 1:
+                    crypt.output = arg;
+                    use_output_file = true;
+                    break;
                 default:
                     throw std::runtime_error(
                         "Error: Too many positional arguments.\n");
@@ -208,6 +277,8 @@ crypt_t get_crypt_args(const std::span<char*>& in_cmd_arg) {
                 throw std::runtime_error("Error: Invalid option.\n");
             case opt_id::help: throw show_help();
             case opt_id::version: throw show_version();
+            case opt_id::stdin_: crypt.use_stdin = true; break;
+            case opt_id::stdout_: crypt.use_stdout = true; break;
             case opt_id::pad:
                 crypt.pad.path = arg;
                 found_opt = default_opt;
@@ -219,6 +290,16 @@ crypt_t get_crypt_args(const std::span<char*>& in_cmd_arg) {
                 continue;
             default: throw std::runtime_error("Error: Invalid option id.\n");
         }
+    }
+
+    if (crypt.use_stdin && use_input_file) {
+        throw std::runtime_error(
+            "Error: Cannot receive input from both stdin and a file.\n");
+    }
+
+    if (crypt.use_stdout && use_output_file) {
+        throw std::runtime_error(
+            "Error: Cannot send output to both stdout and a file.\n");
     }
 
     return crypt;
@@ -233,17 +314,28 @@ int main(int argc, char** argv) {
     try {
         crypt_t crypt = get_crypt_args(arg.subspan(1, arg.size() - 1));
 
-        file_r input(crypt.input, 0);
-        file_r pad(crypt.pad.path, crypt.pad.pos);
-        file_w output(crypt.output);
-
-        if ((pad.size() - crypt.pad.pos) < input.size()) {
-            throw std::runtime_error(
-                "The pad file is too small for the given pad position "
-                "and input file.\n");
+        istream input{};
+        if (! crypt.use_stdin) {
+            input = istream{ crypt.input, 0 };
         }
 
-        for (size_t i = 0; i < input.size(); ++i) {
+        istream pad(crypt.pad.path, crypt.pad.pos);
+
+        ostream output{};
+        if (! crypt.use_stdout) {
+            output = ostream{ crypt.output };
+        }
+
+        // TODO: Ensure that the pad size (when also accounting for the
+        // position) does not exceed the size of the input.
+
+        // if ((pad.size() -
+        //     throw std::runtime_error(
+        //         "The pad file is too small for the given pad position "
+        //         "and input file.\n");
+        // }
+
+        for (size_t i = 0; i < pad.size(); ++i) {
             output.put(input.get() ^ pad.get());
         }
     }
@@ -252,6 +344,14 @@ int main(int argc, char** argv) {
     }
     catch (const file_open_error& e) {
         return failure(e.what());
+    }
+    catch (const eof_error& e) {
+        switch (e.get_eof_type()) {
+            case eof::file: return failure(e.what());
+            case eof::stdin_: return success();
+            default:
+                return failure("Invalid eof enum from eof_error exception.\n");
+        }
     }
     catch (const show_help& e) {
         return success(e.what());
@@ -262,3 +362,5 @@ int main(int argc, char** argv) {
 
     return success();
 }
+
+namespace thing {}
--- src/version.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace xor_crypt {
-
-constexpr const char* compiletime_version =
-    "\n   xor_crypt version: 0.0.0\n\n";
-const char* get_runtime_version();
-
-} // namespace xor_crypt
